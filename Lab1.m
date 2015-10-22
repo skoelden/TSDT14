a282e6c335d79f47ffc2bec83a38c0e87a03297c
unknown <skold.erik@gmail.com>
2015-10-22 16:54:04 +0200

    Fixed ACF for lo noise

--- Lab1.m
+++ Lab1.m
@@ -3,8 +3,8 @@ clear all
 clc
 constants
 tic
-RyLoworder = R0*abs((1-a+a*exp(j*2*pi*theta))).^2;
-ryLoworder = (1-2*a+a^2)*R0*not(tau)+(1-a)*a*R0*not(tau-1);
+RyLoworder = R0*abs((1-a+a*exp(-j*2*pi*theta))).^2;
+ryLoworder = (a^2+(1-a)^2)*R0*not(tau)+a*(1-a)*R0*not(tau-1);
 
 RyHighorder = R0*(rectpuls(theta/(2*theta0)) + rectpuls((1-theta)/(2*theta0)));
 ryHighorder = 2*theta0*sinc(2*theta0*tau);
@@ -15,7 +15,7 @@ for i = [2:length(noise)]
     filterednoise(i) = (1-a)*noise(i) + a*noise(i-1);
 end
 
-[bbutter, abutter] = butter(30, 2*theta0);
+[bbutter, abutter] = butter(10, 2*theta0);
 idealfilterednoise = filter(bbutter, abutter, noise);
 
 [BARfilteredACF, BARfilteredPSD] = ACFe(filterednoise, 'bar');
@@ -57,9 +57,9 @@ set(gca,'FontSize',fontSize)
 
 figure(2)
 zeropoint = ceil(length(BARfilteredACF)/2);
-stem(tau,BARfilteredACF(zeropoint:zeropoint+20))
+stem([-20:20],BARfilteredACF(zeropoint-20:zeropoint+20))
 hold on
-stem(tau,ryLoworder(1:length(tau)),'rx'); 
+stem([-20:20],[fliplr(ryLoworder(2:length(tau))), ryLoworder(1:length(tau))],'rx'); 
 hold off
 title('Bartletts estimate for low order filtered noise')
 xlabel('Delay (samples)')
@@ -76,9 +76,9 @@ set(gca,'FontSize',fontSize)
 
 figure(4)
 zeropoint = ceil(length(BARidealfilteredACF)/2);
-stem(tau,BARidealfilteredACF(zeropoint:zeropoint+20))
+stem([-20:20],BARidealfilteredACF(zeropoint-20:zeropoint+20))
 hold on
-stem(tau,ryHighorder(1:length(tau)),'rx'); 
+stem([-20:20],[fliplr(ryHighorder(2:length(tau))), ryHighorder(1:length(tau))],'rx'); 
 hold off
 title('Bartletts estimate for high order filtered noise')
 xlabel('Delay (samples)')
