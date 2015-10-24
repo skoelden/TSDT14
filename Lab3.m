7feafc1b3e87695914926fdd12676b3bc582fac8
marvi154 <margareta.vi@gmail.com>
2015-10-24 17:23:36 +0200

    added plots for lab 3

--- Lab3.m
+++ Lab3.m
@@ -27,7 +27,7 @@ RyDecimated = 1/4*(rectpuls(theta/(2*theta0)) + rectpuls((theta-1/2)/(2*theta0))
 [alternatingACF, alternatingPSD] = ACFe(alternatingnoise, 'bar', 's');
 [decimatedACF, decimatedPSD] = ACFe(decimatednoise, 'bar', 's');
 
-% Calculating the PSD of the signals
+% Calculating the raw PSD of the signals
 [rawalternatingACF, rawalternatingPSD] = ACFe(alternatingnoise, 'bar');
 [rawdecimatedACF, rawdecimatedPSD] = ACFe(decimatednoise, 'bar');
 
@@ -39,7 +39,7 @@ plot(0:1/(length(alternatingPSD)-1):1, alternatingPSD)
 hold on
 plot(0:1/(length(RyAlternating)-1):1, RyAlternating, 'r')
 hold off
-legend('Estimate', 'Theoretical'), title('PSD of alternating signal')
+legend('Estimate', 'Theoretical'), title('PSD of alternating signal,smoothed')
 xlabel('Normalized frequency, \theta')
 set(gca,'FontSize',fontSize)
 
@@ -48,7 +48,31 @@ plot(0:1/(length(decimatedPSD)-1):1, decimatedPSD)
 hold on
 plot(0:1/(length(RyDecimated)-1):1, RyDecimated, 'r')
 hold off
-legend('Estimate', 'Theoretical'), title('PSD of decimated signal')
+legend('Estimate', 'Theoretical'), title('PSD of decimated signal,smoothed')
 xlabel('Normalized frequency, \theta')
 set(gca,'FontSize',fontSize)
 
+figure(3)
+plot(0:1/(length(rawdecimatedPSD)-1):1, rawdecimatedPSD)
+hold on
+plot(0:1/(length(RyDecimated)-1):1, RyDecimated, 'r')
+hold off
+legend('Estimate', 'Theoretical'), title('PSD of decimated signal,raw')
+xlabel('Normalized frequency, \theta')
+set(gca,'FontSize',fontSize)
+
+figure(4)
+plot(0:1/(length(rawalternatingPSD)-1):1, rawalternatingPSD)
+hold on
+plot(0:1/(length(RyAlternating)-1):1, RyAlternating, 'r')
+hold off
+legend('Estimate', 'Theoretical'), title('PSD of decimated signal,raw')
+xlabel('Normalized frequency, \theta')
+set(gca,'FontSize',fontSize)
+
+figure(5)
+plot(0:1/(length(RyHighorder)-1):1, RyHighorder)
+ylim([0, 0.4]), 
+title('PSD of the input')
+xlabel('Normalized frequency, \theta')
+set(gca,'FontSize',fontSize)
